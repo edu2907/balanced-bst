8500a247d233f3342e16ad631ff3b57b2e31049d
eduardo <eduardo.carneiro2907@gmail.com>
2022-07-30 16:55:28 -0300

    Add #insert method

--- lib/tree.rb
+++ lib/tree.rb
@@ -13,14 +13,49 @@ class Tree
     mid = array.size / 2
     root = Node.new(array[mid])
     root.left_child = build_tree(array[0...mid])
-    root.right_child = build_tree(array[(mid+1)...array.size])
+    root.right_child = build_tree(array[(mid + 1)...array.size])
 
     root
+  end
+
+  def insert(value)
+    new_node = Node.new(value)
+    insert_rec(new_node, @root)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end
+
+  private
+
+  def insert_rec(new_node, current_node, root_node = current_node, path = nil)
+    if current_node.nil?
+      link_node(new_node, root_node, path)
+      return nil
+    end
+    next_path = node_path(current_node, new_node)
+    next_node = pick_child(current_node, next_path)
+    insert_rec(new_node, next_node, current_node, next_path)
+  end
+
+  def node_path(root_node, new_node)
+    new_node < root_node ? 'left' : 'right'
+  end
+
+  def pick_child(node, path)
+    case path
+    when 'left' then node.left_child
+    when 'right' then node.right_child
+    end
+  end
+
+  def link_node(new_node, root_node, path)
+    case path
+    when 'left' then root_node.left_child = new_node
+    when 'right' then root_node.right_child = new_node
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 require_relative 'lib/tree'
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-my_tree = Tree.new(arr)
+my_tree = Tree.new(arr.uniq.sort)
+my_tree.pretty_print
+
+my_tree.insert(10)
+
 my_tree.pretty_print
